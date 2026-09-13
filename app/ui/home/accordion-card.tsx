787c920b57dc465091bcc809c1270b4c0f34e093
Nam Nguyen <145630409+namhnguyen0103@users.noreply.github.com>
2026-09-13 13:36:25 -0400

    update: update global colors

--- app/ui/home/accordion-card.tsx
+++ app/ui/home/accordion-card.tsx
@@ -17,9 +17,7 @@ export default function AccordionCard({ header, content } : AccordionCardProps) 
     
     return (
         <button className='w-full group cursor-pointer active:scale-99' onClick={userClick}>
-            <div className={clsx('relative bg-white px-[20px] py-[16px] ring-[1.5px] rounded-xl ring-inset',
-                expanded ? "ring-blue-300" : "ring-gray-200"
-            )}>
+            <div className='py-[16px] px-[16px] rounded-xl ring-[2px] ring-gray-50 ring-inset'>
                 <div>{header}</div>
                 <div className={expanded ? "block" : "hidden"}>{content}</div>
                 <span className="absolute material-symbols-rounded text-gray-700 leading-[16px] top-[16px] right-[20px]" style={{ fontSize: '20px', fontVariationSettings: "'opsz' 20" }}>{expanded ? "collapse_all" : "expand_all"}</span>
